f8db0b727ba09fc79ed49032cd6fd67ea40da9f6
eva3a <evaduabelas3a@gmail.com>
2021-10-11 10:34:09 +0700

    Upload Awal Project

---
+++ application/controllers/Contoh.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh extends CI_Controller {
+
+	
+	public function index()
+	{
+		echo "<h1> Ayo semangat belajar Membuat Web. Pasti Bisa </h2>";
+	}
+}
